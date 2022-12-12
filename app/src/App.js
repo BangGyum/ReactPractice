2ae85fbaa7c5e4eac607eb5420ee5fd6f06c717c
BangGyum <83632850+BangGyum@users.noreply.github.com>
2022-12-12 23:57:05 +0900

    todo 1

--- app/src/App.js
+++ app/src/App.js
@@ -4,7 +4,50 @@ import { useState } from "react";
 import { useEffect } from "react"; //첫번째 인자 argument는 한번만 실해앟고 싶은 코드
 
 function App() {
-  const [counter,setValue]= useState(0);
+  const [todo, setTodo]= useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();//메서드는 어떤 이벤트를 명시적으로 처리하지 않은 경우, 해당 이벤트에 대한 사용자 에이전트의 기본 동작을 실행하지 않도록 지정gksek.
+    
+    if (todo ===""){
+      return;
+    }
+    console.log("onSubmit");
+    
+    setTodo("");
+    setTodos(currentArray => [todo,...currentArray]);
+    console.log(todos);
+  }
+  console.log(todos.map((item,index) => <li key={index}>{item}</li>));
+  return (
+
+    <div>
+      <h1>My To Dos {todos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={todo} type="text" placeholder="Write your to do.."/>
+        <button>Add todo</button>
+      </form>
+      <hr/>
+      <ul>
+        {todos.map((item,index) => <li key={index}>{item}</li>)} 
+      </ul>
+    </div>
+  ); //.map(()=>"?")은 리스트에 대해, 원소 수만큼 실행되고, 결과물들을 다시 재배치하는데 (전부 물음표로), 그럼 기존게 없어지잖아
+    // .map((item)=>item.ToUpperCase()) 하면 그대로 return함
+    // 이러면 key를 넣으라 하는데 react가 식별하는데 도움을 주기때문, 또 변경 추가 삭제할때 쉽,
+    // map argu 보면 첫번재는 value (각각의 todo), 두번째는 index이다. 
+}
+
+export default App;
+
+/*
+  react.js 는 변화가 일어날 때만 refresh/ 근데 전체 컴포넌트가 다시 refresh 되기 때문에 useEffect 로, 바뀔때만 실행되는 코드 작성 가능
+  function useEffect(effect: EffectCallback, deps?: DependencyList): void
+                        DependencyList는 reactjs가 지켜봐야 하는 리스트.
+
+
+                          const [counter,setValue]= useState(0);
   const [keyword, setKeyword] = useState("");
   const onChange = (event) => setKeyword(event.target.value);
   const onClick=() => setValue((prev) => prev + 1);
@@ -38,12 +81,29 @@ function App() {
        <Btn text="a"/>
     </div>
   );
+*/
+
+/* cleanup function, useEffect 사용때, 해당 컴포넌트가 지워질 시에 return값을 줌으로써 사용. but 잘안씀
+
+
+function Hello() {
+  useEffect(() =>{
+    console.log("created");
+    return () => console.log("destroyed: :( "); //언제 파괴됐는지 확인가능, 파괴될때 function 사용가능
+  },[]);
+  return <h1>Hello</h1>;
 }
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick =() =>{
+    setShowing((prev) => !prev);
+  }
+  return (
 
-export default App;
-
-/*
-  react.js 는 변화가 일어날 때만 refresh/ 근데 전체 컴포넌트가 다시 refresh 되기 때문에 useEffect 로, 바뀔때만 실행되는 코드 작성 가능
-  function useEffect(effect: EffectCallback, deps?: DependencyList): void
-                        DependencyList는 reactjs가 지켜봐야 하는 리스트.
+    <div>
+      {showing ? <Hello /> :null};
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
 */
