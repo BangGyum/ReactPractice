4ff2d5dedd9dcf1f4be087965464fc5801c65153
BangGyum <83632850+BangGyum@users.noreply.github.com>
2022-12-13 16:51:51 +0900

    coin list

--- app/src/App.js
+++ app/src/App.js
@@ -4,39 +4,31 @@ import { useState } from "react";
 import { useEffect } from "react"; //첫번째 인자 argument는 한번만 실해앟고 싶은 코드
 
 function App() {
-  const [todo, setTodo]= useState("");
-  const [todos, setTodos] = useState([]);
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();//메서드는 어떤 이벤트를 명시적으로 처리하지 않은 경우, 해당 이벤트에 대한 사용자 에이전트의 기본 동작을 실행하지 않도록 지정gksek.
-    
-    if (todo ===""){
-      return;
-    }
-    console.log("onSubmit");
-    
-    setTodo("");
-    setTodos(currentArray => [todo,...currentArray]);
-    console.log(todos);
-  }
-  console.log(todos.map((item,index) => <li key={index}>{item}</li>));
-  return (
-
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then(response => response.json())
+    //.then(json => console.log(json)
+    .then(json => {
+      setCoins(json);
+      setLoading(false);
+    });
+  },[]);
+  return (  
     <div>
-      <h1>My To Dos {todos.length}</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={todo} type="text" placeholder="Write your to do.."/>
-        <button>Add todo</button>
-      </form>
-      <hr/>
-      <ul>
-        {todos.map((item,index) => <li key={index}>{item}</li>)} 
-      </ul>
+      <h1>The Coins!{coins.length} </h1>
+      {loading ?
+       <strong>Loading</strong>
+        :
+        <select>
+        {coins.map((coin) => <option>{coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}USD</option>)}
+      </select>
+        }
+
+
     </div>
-  ); //.map(()=>"?")은 리스트에 대해, 원소 수만큼 실행되고, 결과물들을 다시 재배치하는데 (전부 물음표로), 그럼 기존게 없어지잖아
-    // .map((item)=>item.ToUpperCase()) 하면 그대로 return함
-    // 이러면 key를 넣으라 하는데 react가 식별하는데 도움을 주기때문, 또 변경 추가 삭제할때 쉽,
-    // map argu 보면 첫번재는 value (각각의 todo), 두번째는 index이다. 
+  ); //
 }
 
 export default App;
@@ -106,4 +98,44 @@ function App() {
     </div>
   );
 }
+*/
+
+/*
+ const [todo, setTodo]= useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();//메서드는 어떤 이벤트를 명시적으로 처리하지 않은 경우, 해당 이벤트에 대한 사용자 에이전트의 기본 동작을 실행하지 않도록 지정gksek.
+    
+    if (todo ===""){
+      return;
+    }
+    console.log("onSubmit");
+    
+    setTodo("");
+    setTodos(currentArray => [todo,...currentArray]);
+    console.log(todos);
+  }
+  console.log(todos.map((item,index) => <li key={index}>{item}</li>));
+  return (  
+
+    <div>
+      <h1>My To Dos {todos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={todo} type="text" placeholder="Write your to do.."/>
+        <button>Add todo</button>
+      </form>
+      <hr/>
+      <ul>
+        {todos.map((item,index) => <li key={index}>{item}</li>)} 
+      </ul>
+    </div>
+  ); //.map(()=>"?")은 리스트에 대해, 원소 수만큼 실행되고, 결과물들을 다시 재배치하는데 (전부 물음표로), 그럼 기존게 없어지잖아
+    // .map((item)=>item.ToUpperCase()) 하면 그대로 return함
+    // 이러면 key를 넣으라 하는데 react가 식별하는데 도움을 주기때문, 또 변경 추가 삭제할때 쉽,
+    // map argu 보면 첫번재는 value (각각의 todo), 두번째는 index이다. 
+*/
+
+/*Fetch API는 HTTP 파이프라인을 구성하는 요청과 응답 등의 요소를 JavaScript에서 접근하고 조작할 수 있는 인터페이스를 제공합니다. 
+
 */
